2e4572e7e99b0ec557de150e408f1b7ba8635736
Aaron Hayslip <aaronhayslip@Aarons-MacBook-Pro.local>
2022-07-21 13:46:57 -0500

    WIP

---
+++ src/MainMint.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useState } from 'react';
+import { ethers, BigNumber } from 'ethers';
+import roboPunksNFT from './RoboPunksNFT.json';
+
+const roboPunksNFTAddress = "0x2bF90F2c25045e35611936b28Cb6C384A18A4995";
+
+const MainMint = ({ accounts, setAccounts }) => {
+  const [mintAmount, setMintAmount] = useState(1);
+  const isConnected = Boolean(accounts[0]);
+
+  async function handleMint() {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        roboPunksNFTAddress,
+        roboPunksNFT.abi,
+        signer
+      );
+      try {
+        const response = await contract.mint(BigNumber.from(mintAmount),  {
+          value: ethers.utils.parseEther((0.002 * mintAmount).toString())
+        });
+        console.log('response: ', response);
+      } catch (err) {
+        console.log("error: ", err)
+      }
+    }
+  }
+
+  const handleDecrement = () => {
+    if (mintAmount <= 1) return;
+    setMintAmount(mintAmount - 1);
+  }
+
+  const handleIncrement = () => {
+    if (mintAmount >= 3) return;
+    setMintAmount(mintAmount + 1);
+  }
+
+  return (
+    <div>
+       <h1>RoboPunks</h1>
+       <p>It's 2078. Can the RoboPunks NFT save humans from destructive rampant NFT speculation? Mint Robopunks to find out.</p>
+       {isConnected ? (
+         <div>
+           <div>
+             <button onClick={handleDecrement}>-</button>
+             <input type='number' value={mintAmount} />
+             <button onClick={handleIncrement}>+</button>
+           </div>
+           <button onClick={handleMint}>Mint Now</button>
+         </div>
+       ) : (
+         <p>You must be connected to Mint.</p>
+       )}
+    </div>
+  );
+};
+
+export default MainMint;
